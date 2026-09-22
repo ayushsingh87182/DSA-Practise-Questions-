354ea32629542da94dd6f7c943d3a605f4b215c8
Ayush Singh <ayush.ranchi.2506@gmail.com>
2026-09-22 23:35:23 +0530

    22/09/26

---
+++ TwoSum.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class TwoSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.add(Arrays.asList(
+                            nums[i], nums[j], nums[left], nums[right]
+                        ));
+
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = scanner.nextInt();
+
+        TwoSum solution = new TwoSum();
+        List<List<Integer>> result = solution.fourSum(nums, target);
+
+        System.out.println("Quadruplets:");
+        for (List<Integer> quadruplet : result) {
+            System.out.println(quadruplet);
+        }
+
+        scanner.close();
+    }
+}
