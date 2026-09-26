4bcefe02f3a7ab1cf12429483756b5438982e16e
Ayush Singh <ayush.ranchi.2506@gmail.com>
2026-09-26 16:33:29 +0530

    26/09/26

---
+++ TilingProblems.java
@@ -0,0 +1,18 @@
+public class TilingProblems {
+    public static int tilingProblem(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        //vertical choice
+        int fnm1 = tilingProblem(n-1);
+        //horizontal choice
+        int fnm2 = tilingProblem(n-2);
+
+        int totalWays = fnm1 + fnm2;
+        return totalWays;
+    }
+    public static void main(String args[]){
+        int n = 4;
+        System.out.println(tilingProblem(n));
+    }
+}
